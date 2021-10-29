198a5270528b146d7062b8edebd6924254263074
Sean Connolly <sconnolly17@gmail.com>
2021-10-29 09:07:56 -0600

    More print statements and demonstrable repro case

--- lib/screens/channel_list_screen.dart
+++ lib/screens/channel_list_screen.dart
@@ -3,7 +3,7 @@ import 'package:sendbird_flutter_issues/screens/channel_screen.dart';
 import 'package:sendbird_flutter_issues/sendbird_service.dart';
 import 'package:sendbird_sdk/sendbird_sdk.dart';
 
-class ChannelListScreen extends StatelessWidget {
+class ChannelListScreen extends StatefulWidget {
   static const routeName = '/channel-list';
 
   final SendbirdService _sendbird;
@@ -11,14 +11,29 @@ class ChannelListScreen extends StatelessWidget {
   const ChannelListScreen(this._sendbird, {Key? key}) : super(key: key);
 
   @override
+  State<ChannelListScreen> createState() => _ChannelListScreenState();
+}
+
+class _ChannelListScreenState extends State<ChannelListScreen> {
+  @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<GroupChannel>>(
-      future: _sendbird.getGroupChannels(),
+      future: widget._sendbird.getGroupChannels(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) {
           return Scaffold(body: Center(child: CircularProgressIndicator()));
         }
         return Scaffold(
+          appBar: AppBar(
+            title: Text('Sendbird'),
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    setState(() {});
+                  },
+                  icon: Icon(Icons.refresh))
+            ],
+          ),
           body: ListView.separated(
             itemBuilder: (context, i) {
               final groupChannel = snapshot.data![i];
--- lib/sendbird_service.dart
+++ lib/sendbird_service.dart
@@ -4,9 +4,13 @@ import 'package:sendbird_sdk/sendbird_sdk.dart';
 class SendbirdService {
   final SendbirdSdk _sendbirdSdk;
 
-  SendbirdService(this._sendbirdSdk);
+  SendbirdService(this._sendbirdSdk) {
+    _sendbirdSdk.addConnectionEventHandler(
+        'CONNECTION_EVENT_HANDLER', MyConnectionEventListener());
+  }
 
   Future<List<GroupChannel>> getGroupChannels() async {
+    print('>>> getGroupChannels()');
     await _connect();
     final query = GroupChannelListQuery()
       ..memberStateFilter = MemberStateFilter.joined
@@ -16,6 +20,7 @@ class SendbirdService {
   }
 
   Future<GroupChannel> getGroupChannel(String channelUrl) async {
+    print('>>> getGroupChannel($channelUrl)');
     await _connect();
     return GroupChannel.getChannel(channelUrl);
   }
@@ -23,11 +28,40 @@ class SendbirdService {
   Future<void> _connect() async {
     final testUserId = dotenv.env['TEST_USER_ID'];
     if (testUserId != null) {
-      await _sendbirdSdk.connect(testUserId);
+      await _sendbirdSdk.connect(testUserId).catchError((err) {
+        print('>>> Sendbird connect() error!');
+        throw err;
+      });
       print(
           '>>> connect awaited | state: ${_sendbirdSdk.getConnectionState()}');
       return;
     }
     throw 'TEST_USER_ID is required. Please add to your .env';
+  }
+}
+
+class MyConnectionEventListener extends ConnectionEventHandler {
+  @override
+  void onReconnectionStarted() {
+    print('>>> onReconnectionStarted');
+    super.onReconnectionStarted();
+  }
+
+  @override
+  void onReconnectionCanceled() {
+    print('>>> onReconnectionCanceled');
+    super.onReconnectionCanceled();
+  }
+
+  @override
+  void onReconnectionFailed() {
+    print('>>> onReconnectionFailed');
+    super.onReconnectionFailed();
+  }
+
+  @override
+  void onReconnectionSucceeded() {
+    print('>>> onReconnectionSucceeded');
+    super.onReconnectionSucceeded();
   }
 }
